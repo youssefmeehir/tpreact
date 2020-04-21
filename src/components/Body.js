fa0f82adfd893b03ec9e2c9786829d629200fb19
EL MERROUNI Marouane <melmerrouni@norsys.fr>
2020-04-21 11:01:17 +0000

    feta: react bootstrap OK

---
+++ src/components/Body.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Body = () => {
+  return (
+    <body>
+      <div>
+        <input type="text" />
+        <button>search</button>
+      </div>
+      <div>
+        <div>book1</div>
+        <div>book2</div>
+        <div>book3</div>
+      </div>
+    </body>
+  )
+};
+
+export default Body;
